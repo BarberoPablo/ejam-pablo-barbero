fdeb51c210a2d96d683c2fd65f80585d0cd4117a
Pablo Barbero <pablobarbero220@gmail.com>
2024-06-26 19:59:34 -0300

    feat: message title

--- src/routes/ProductDetails/ProductDetails.tsx
+++ src/routes/ProductDetails/ProductDetails.tsx
@@ -1,10 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Sponsors from "../../components/Sponsors";
 
 export default function ProductDetails() {
   return (
     <Box maxWidth={1245} m="0 auto">
       <Sponsors />
+      <Stack sx={{ alignItems: "center" }}>
+        <Typography variant="title">Wait ! your order in progress.</Typography>
+        <Typography variant="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing</Typography>
+      </Stack>
     </Box>
   );
 }
