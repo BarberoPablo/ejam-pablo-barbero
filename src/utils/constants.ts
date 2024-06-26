f582a82a757b7b7001ed1b95153d9577b2dc4afd
Pablo Barbero <pablobarbero220@gmail.com>
2024-06-26 19:25:46 -0300

    feat: top bar and icons

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,11 @@
-import checkmark from "../assets/checkmark-starburst-20-regular.svg";
-import truck from "../assets/truck-light.svg";
-import heart from "../assets/cards-heart-outline.svg";
-import spiralArrow from "../assets/arrow-sync-checkmark-20-regular.svg";
+import checkmark from "../assets/icons/checkmark.svg";
+import truck from "../assets/icons/truck-light.svg";
+import heart from "../assets/icons/heart-outline.svg";
+import spiralArrow from "../assets/icons/arrow-sync.svg";
 
 export const usps = [
-  { logo: checkmark, text: "30-DAY SATISFACTION GUARANTEE" },
+  { logo: checkmark, text: "30-Day Satisfaction Guarantee" },
   { logo: truck, text: "Free delivery on orders over $40.00" },
-  { logo: heart, text: "50.000+ HAPPY CUSTOMERS" },
+  { logo: heart, text: "50.000+ Happy Customers" },
   { logo: spiralArrow, text: "100% Money Back Guarantee" },
 ];
