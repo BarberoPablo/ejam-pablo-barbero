ea300d3cd0064d04894f334a91bbe81ad505d06f
Pablo Barbero <pablobarbero220@gmail.com>
2024-06-26 17:20:47 -0300

    feat: usps object properties

---
+++ src/components/Usps.tsx
@@ -0,0 +1,3 @@
+export default function Usps() {
+  return <div>Usps</div>;
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+import checkmark from "../assets/checkmark-starburst-20-regular.svg";
+import truck from "../assets/truck-light.svg";
+import heart from "../assets/cards-heart-outline.svg";
+import spiralArrow from "../assets/arrow-sync-checkmark-20-regular.svg";
+
+export const usps = [
+  { logo: checkmark, text: "30-DAY SATISFACTION GUARANTEE" },
+  { logo: truck, text: "Free delivery on orders over $40.00" },
+  { logo: heart, text: "50.000+ HAPPY CUSTOMERS" },
+  { logo: spiralArrow, text: "100% Money Back Guarantee" },
+];
